451540a20f795830d659a225c30bfdb2481f0ea4
Dennis-sai <dennissremsai@gmail.com>
2023-12-12 14:33:18 +0100

    Add spec model folder for testing models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+
+  it "is not valid without a title" 
+  it "is not valid without a name"
+  it "is not valid without a last_name"
+end
