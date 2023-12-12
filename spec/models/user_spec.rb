d23ca75b7fd6ae5ca71a0a9172dd0c1caff7e159
Dennis-sai <dennissremsai@gmail.com>
2023-12-12 15:13:48 +0100

    Remove debugger

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { build(:user) }
  
-  it "has first_name" do
-    binding.irb
-  end
+  it "has first_name" 
   it "has last_name"
   it "has title"
+  it "has email"
   it "has mobile_number"
   it "has description"
 end
